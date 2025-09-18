f4b1b8a15eb6e93669838ebc1893da4bbd030f29
Шумилкин Никита Андреевич <s22270035@unn.ru>
2025-09-18 10:56:11 +0300

    Исправлен6

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-app = Flask (__name__)
+app = Flask(__name__)
+
 @app.route('/')
 def hello():
     return "Hi!"
-@app.route('health')
+
+@app.route('/health')
 def health():
     return "ok", 200
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port = 5000)
